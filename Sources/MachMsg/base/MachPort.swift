bb15f9ee9a83fa90154349bbb813ac6ec66407b0
Noah Gregory <noahmgregory@gmail.com>
2024-08-30 03:02:38 -0400

    add port attributes

--- Sources/MachMsg/base/MachPort.swift
+++ Sources/MachMsg/base/MachPort.swift
@@ -170,6 +170,64 @@ open class MachPort: RawRepresentable, Hashable {
         KernelObject(port: self)
     }
 
+    /// An attribute of a Mach port.
+    public enum Attribute: mach_port_flavor_t, CBinIntMacroEnum {
+        case limitsInfo = 1
+        case receiveStatus = 2
+        case dnrequestsSize = 3
+        case tempowner = 4
+        case importanceReceiver = 5
+        case denapReceiver = 6
+        case infoExt = 7
+        case guardInfo = 8
+        case serviceThrottled = 9
+        public var cMacroName: String {
+            "MACH_PORT_"
+                + "\(self)"
+                .replacingOccurrences(
+                    of: "([a-z])([A-Z])", with: "$1_$2", options: .regularExpression
+                )
+                .uppercased()
+        }
+    }
+
+    /// The attributes of the Mach port.
+    public struct Attributes {
+        internal let rawPort: mach_port_t
+        public subscript<T>(flavor: Attribute, as: T.Type) -> T? {
+            get {
+                var count = mach_msg_type_number_t.max
+                let info = mach_port_info_t.allocate(capacity: Int(count))
+                let ret = mach_port_get_attributes(
+                    mach_task_self_, self.rawPort, flavor.rawValue, info, &count
+                )
+                guard ret == KERN_SUCCESS else { return nil }
+                return info.withMemoryRebound(to: T.self, capacity: Int(count)) { $0.pointee }
+            }
+            set(newValue) {
+                guard newValue != nil else { return }
+                // The kernel will return `MIG_ARRAY_TOO_LARGE` if the count is too large.
+                let count = mach_msg_type_number_t(0x11)
+                let info = mach_port_info_t.allocate(capacity: Int(count))
+                info.withMemoryRebound(to: T.self, capacity: Int(count)) { $0.pointee = newValue! }
+                let ret = mach_port_set_attributes(
+                    mach_task_self_, self.rawPort, flavor.rawValue, info, count
+                )
+                guard ret == KERN_SUCCESS else { return }
+            }
+        }
+    }
+
+    /// The attributes of the Mach port.
+    public var attributes: Attributes {
+        get {
+            Attributes(rawPort: self.rawValue)
+        }
+        set {
+            // This is a no-op, as the subscript setter is used to set the attributes. This is just here to allow the user to set the attributes.
+        }
+    }
+
     /// A null Mach port.
     public static var null: Self {
         Self(rawValue: mach_port_t(MACH_PORT_NULL))
