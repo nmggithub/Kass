2319c116926e7d68afff87c69344c07e5e3134ec
Noah Gregory <noahmgregory@gmail.com>
2024-09-02 19:07:44 -0400

    rewrite header struct

--- Sources/MachMsg/base/header/MachMessageHeader.swift
+++ Sources/MachMsg/base/header/MachMessageHeader.swift
@@ -1,163 +1,81 @@
 import CCompat
 import MachO
+import MachPort
 
-private let MACH_MSGH_BITS_PORTS_MASK = mach_msg_type_name_t(
-    MACH_MSGH_BITS_REMOTE_MASK | MACH_MSGH_BITS_LOCAL_MASK | MACH_MSGH_BITS_VOUCHER_MASK)
+/// A Mach message header.
+public struct MachMessageHeader: RawRepresentable {
+    /// The raw message header.
+    public var rawValue: mach_msg_header_t {
+        mach_msg_header_t(
+            msgh_bits: self.bits.rawValue,
+            msgh_size: self.messageSize,
+            msgh_remote_port: self.remotePort.rawValue,
+            msgh_local_port: self.localPort.rawValue,
+            msgh_voucher_port: self.voucherPort.rawValue,
+            msgh_id: self.messageID
+        )
+    }
+    /// Represent an existing raw message header.
+    /// - Parameter rawValue: The raw message header.
+    public init(rawValue: mach_msg_header_t) {
+        self.bits = MachMessageHeaderBits(rawValue: rawValue.msgh_bits)
+        self.messageSize = rawValue.msgh_size
+        self.remotePort = MachPort(rawValue: rawValue.msgh_remote_port)
+        self.localPort = MachPort(rawValue: rawValue.msgh_local_port)
+        self.voucherPort = MachPort(rawValue: rawValue.msgh_voucher_port)
+        self.messageID = rawValue.msgh_id
+    }
+    /// The configuration bits.
+    var bits: MachMessageHeaderBits
+    /// The advertised message size.
+    var messageSize: mach_msg_size_t
+    /// The remote port.
+    var remotePort: MachPort
+    /// The local port.
+    var localPort: MachPort
+    /// The voucher port.
+    var voucherPort: MachPort
+    /// The message ID.
+    var messageID: mach_msg_id_t
+    /// Create a new message header.
+    public init() { self.init(rawValue: mach_msg_header_t()) }
+}
 
-/// A class representing the header of a Mach message.
-public class MachMessageHeader {
-
-    /// A struct representing the configuration bits for the message.
-    public struct Bits: RawRepresentable {
-        public typealias RawValue = mach_msg_bits_t
-
-        /// The raw value of the configuration bits.
-        public var rawValue: RawValue {
-            get { self._remote | self._local << 8 | self._voucher << 16 | self._other }
-            set { self = .init(rawValue: newValue) }
-        }
-
-        private var _remote: mach_msg_type_name_t
-        private var _local: mach_msg_type_name_t
-        private var _voucher: mach_msg_type_name_t
-        private var _other: mach_msg_type_name_t
-
-        /// The remote port disposition.
-        public var remote: mach_msg_type_name_t {
-            get { self._remote & mach_msg_type_name_t(MACH_MSGH_BITS_REMOTE_MASK) }
-            set { self._remote = newValue & mach_msg_type_name_t(MACH_MSGH_BITS_REMOTE_MASK) }
-        }
-        /// The local port disposition.
-        public var local: mach_msg_type_name_t {
-            get { self._local & mach_msg_type_name_t(MACH_MSGH_BITS_LOCAL_MASK >> 8) }
-            set {
-                self._local = newValue & mach_msg_type_name_t(MACH_MSGH_BITS_LOCAL_MASK >> 8)
-            }
-        }
-        /// The voucher disposition.
-        public var voucher: mach_msg_type_name_t {
-            get { self._voucher & mach_msg_type_name_t(MACH_MSGH_BITS_VOUCHER_MASK >> 16) }
-            set {
-                self._voucher =
-                    newValue & mach_msg_type_name_t(MACH_MSGH_BITS_VOUCHER_MASK >> 16)
-            }
-        }
-        /// The other flags.
-        public var other: mach_msg_type_name_t {
-            get { self._other & mach_msg_type_name_t(~MACH_MSGH_BITS_PORTS_MASK) }
-            set { self._other = newValue & mach_msg_type_name_t(~MACH_MSGH_BITS_PORTS_MASK) }
-        }
-
-        /// Initialize a new configuration bits struct.
-        public init() {
-            self.init(remote: 0, local: 0, voucher: 0, other: 0)
-        }
-        /// Initialize a new configuration bits struct.
-        /// - Parameter bits: The bits to initialize with.
-        public init(rawValue bits: mach_msg_bits_t) {
-            self.init(
-                remote: bits & mach_msg_type_name_t(MACH_MSGH_BITS_REMOTE_MASK),
-                local: (bits & mach_msg_type_name_t(MACH_MSGH_BITS_LOCAL_MASK)) >> 8,
-                voucher: (bits & mach_msg_type_name_t(MACH_MSGH_BITS_VOUCHER_MASK)) >> 16,
-                other: mach_msg_type_name_t(bits & ~MACH_MSGH_BITS_PORTS_MASK)
+extension MachMessageHeader {
+    /// The local port with an associated right type.
+    var localMessagePort: MachMessagePort {
+        get {
+            MachMessagePort(
+                rawPort: self.localPort.rawValue, disposition: self.bits.localPortDisposition
             )
         }
-        /// Initialize a new configuration bits struct.
-        /// - Parameters:
-        ///   - remote: The remote port disposition.
-        ///   - local: The local port disposition.
-        ///   - voucher: The voucher disposition.
-        ///   - other: The other flags.
-        public init(
-            remote: mach_msg_type_name_t, local: mach_msg_type_name_t,
-            voucher: mach_msg_type_name_t, other: mach_msg_type_name_t
-        ) {
-            self._remote = remote & mach_msg_type_name_t(MACH_MSGH_BITS_REMOTE_MASK)
-            self._local = local & mach_msg_type_name_t(MACH_MSGH_BITS_LOCAL_MASK >> 8)
-            self._voucher = voucher & mach_msg_type_name_t(MACH_MSGH_BITS_VOUCHER_MASK >> 16)
-            self._other = other & mach_msg_type_name_t(~MACH_MSGH_BITS_PORTS_MASK)
+        set {
+            self.localPort.rawValue = newValue.rawValue
+            self.bits.localPortDisposition = newValue.disposition
         }
     }
-
-    /// The pointer to the mach_msg_header_t struct.
-    let pointer: UnsafeMutablePointer<mach_msg_header_t>
-
-    /// Operate on the message header with an unsafe mutable pointer.
-    /// - Parameter body: The closure to execute.
-    /// - Returns: The result of the closure.
-    /// - Throws: Any error thrown by the closure.
-    /// - Warning: The pointer must not be deallocated while the closure is executing.
-    /// - Warning: Only use this method if you know what you are doing.
-    public func withUnsafeMutablePointer<T>(
-        _ body: (UnsafeMutablePointer<mach_msg_header_t>) throws -> T
-    ) rethrows -> T {
-        try body(UnsafeMutablePointer(self.pointer))
-    }
-
-    /// The configuration bits for the message.
-    public var bits: Bits {
-        get { Bits(rawValue: self.pointer.pointee.msgh_bits) }
-        set { self.pointer.pointee.msgh_bits = newValue.rawValue }
-    }
-
-    /// The size of the received message data in bytes. It is 0 for sent messages.
-    /// - Note: This excludes any alignment padding between the payload and the trailer and the trailer itself.
-    public var messageSize: mach_msg_size_t {
-        self.pointer.pointee.msgh_size
-    }
-
-    /// The port right being sent to.
-    public var remotePort: MachMessagePort {
+    /// The remote port with an associated right type.
+    var remoteMessagePort: MachMessagePort {
         get {
             MachMessagePort(
-                rawPort: self.pointer.pointee.msgh_remote_port,
-                disposition: .init(rawValue: self.bits.remote) ?? .unknown)
+                rawPort: self.remotePort.rawValue, disposition: self.bits.remotePortDisposition
+            )
         }
         set {
-            self.pointer.pointee.msgh_remote_port = newValue.rawValue
-            self.bits.remote = newValue.disposition.rawValue
+            self.remotePort.rawValue = newValue.rawValue
+            self.bits.remotePortDisposition = newValue.disposition
         }
     }
-
-    /// The port right being sent from.
-    public var localPort: MachMessagePort {
+    /// The voucher port with an associated right type.
+    var voucherMessagePort: MachMessagePort {
         get {
             MachMessagePort(
-                rawPort: self.pointer.pointee.msgh_local_port,
-                disposition: .init(rawValue: self.bits.local) ?? .unknown)
+                rawPort: self.voucherPort.rawValue, disposition: self.bits.voucherPortDisposition
+            )
         }
         set {
-            self.pointer.pointee.msgh_local_port = newValue.rawValue
-            self.bits.local = newValue.disposition.rawValue
+            self.voucherPort.rawValue = newValue.rawValue
+            self.bits.voucherPortDisposition = newValue.disposition
         }
-    }
-
-    /// The voucher port.
-    public var voucherPort: MachMessagePort {
-        get {
-            MachMessagePort(
-                rawPort: self.pointer.pointee.msgh_voucher_port,
-                disposition: .init(rawValue: self.bits.voucher) ?? .unknown)
-        }
-        set {
-            self.pointer.pointee.msgh_voucher_port = newValue.rawValue
-            self.bits.voucher = newValue.disposition.rawValue
-        }
-    }
-
-    /// The user-defined message ID.
-    public var messageID: mach_msg_id_t {
-        get { self.pointer.pointee.msgh_id }
-        set { self.pointer.pointee.msgh_id = newValue }
-    }
-
-    /// Initializes a new Mach message header.
-    /// - Parameter pointer: The pointer to the mach_msg_header_t struct.
-    /// - Warning: The pointer must be deallocated manually by the caller.
-    init(
-        pointer: UnsafeMutablePointer<mach_msg_header_t>
-    ) {
-        self.pointer = pointer
-        self.pointer.initialize(repeating: mach_msg_header_t(), count: 1)
     }
 }
---
+++ Sources/MachMsg/base/header/MachMessageHeaderBits.swift
@@ -0,0 +1,56 @@
+import MachO
+import MachPort
+
+private let MACH_MSGH_BITS_PORTS_MASK = UInt32(
+    MACH_MSGH_BITS_REMOTE_MASK | MACH_MSGH_BITS_LOCAL_MASK | MACH_MSGH_BITS_VOUCHER_MASK
+)
+
+/// The configuration bits of a Mach message header.
+public struct MachMessageHeaderBits: RawRepresentable {
+    /// The raw configuration bits.
+    public var rawValue: mach_msg_bits_t {
+        (self.otherBits & ~MACH_MSGH_BITS_PORTS_MASK)
+            | self.remoteBits
+            | self.localBits << 8
+            | self.voucherBits << 16
+    }
+    /// Represent existing raw configuration bits.
+    /// - Parameter rawValue: The raw configuration bits.
+    public init(rawValue: mach_msg_bits_t) {
+        self.remoteBits = rawValue & mach_msg_type_name_t(MACH_MSGH_BITS_REMOTE_MASK)
+        self.localBits = (rawValue & mach_msg_type_name_t(MACH_MSGH_BITS_LOCAL_MASK)) >> 8
+        self.voucherBits = (rawValue & mach_msg_type_name_t(MACH_MSGH_BITS_VOUCHER_MASK)) >> 16
+        self.otherBits = rawValue & ~MACH_MSGH_BITS_PORTS_MASK
+    }
+
+    /// The raw remote port disposition.
+    var remoteBits: mach_msg_type_name_t
+    /// The raw local port disposition.
+    var localBits: mach_msg_type_name_t
+    /// The raw voucher port disposition.
+    var voucherBits: mach_msg_type_name_t
+    /// The other configuration bits.
+    var otherBits: UInt32
+}
+
+extension MachMessageHeaderBits {
+    /// The remote port disposition.
+    var remotePortDisposition: MachPortDisposition {
+        get { MachPortDisposition(rawValue: self.remoteBits) ?? .unknown }
+        set { self.remoteBits = newValue.rawValue }
+    }
+    /// The local port disposition.
+    var localPortDisposition: MachPortDisposition {
+        get { MachPortDisposition(rawValue: self.localBits) ?? .unknown }
+        set { self.localBits = newValue.rawValue }
+    }
+    /// The voucher port disposition.
+    var voucherPortDisposition: MachPortDisposition {
+        get { MachPortDisposition(rawValue: self.voucherBits) ?? .unknown }
+        set { self.voucherBits = newValue.rawValue }
+    }
+    var isMessageComplex: Bool {
+        get { self.otherBits & MACH_MSGH_BITS_COMPLEX != 0 }
+        set { self.otherBits |= MACH_MSGH_BITS_COMPLEX }
+    }
+}
