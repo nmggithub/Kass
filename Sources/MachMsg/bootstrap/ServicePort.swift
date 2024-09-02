8401629b7d4dec8f200807a449113df77b1b3d69
Noah Gregory <noahmgregory@gmail.com>
2024-09-02 19:13:50 -0400

    rename ServiceConnection to ServicePort

--- Sources/MachMsg/bootstrap/ServicePort.swift
+++ Sources/MachMsg/bootstrap/ServicePort.swift
@@ -2,18 +2,22 @@
 import MachPort
 
 /// A connection to a service.
-open class ServiceConnection: MachConnection {
+open class ServicePort: MachPort {
     /// Create a new connection to a service.
     /// - Parameter serviceName: The name of the service to connect to.
     public init(serviceName: String) throws {
         let bootstrapPort =
             MachTask.current.specialPorts[.bootstrap, BootstrapPort.self]  // attempt to get the bootstrap port functionally
             ?? BootstrapPort(rawValue: bootstrap_port)  // fallback to the kernel-provided symbol for the bootstrap port
-        super.init(port: try bootstrapPort.lookUp(serviceName: serviceName))
+        super.init(rawValue: try bootstrapPort.lookUp(serviceName: serviceName).rawValue)
     }
 
-    // internal to expose to subclasses
-    internal override init(port: MachPort) {
-        super.init(port: port)
+    public required init(nilLiteral: ()) {
+        super.init(nilLiteral: ())
     }
+
+    public required init(rawValue: mach_port_t) {
+        super.init(rawValue: rawValue)
+    }
+
 }
