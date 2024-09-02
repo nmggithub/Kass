fd932ea38662c432ee85297be769e0d5381942c5
Noah Gregory <noahmgregory@gmail.com>
2024-09-02 19:43:44 -0400

    adjust MIG payload definitions

--- Sources/MachMsg/mig/MIGPayload.swift
+++ Sources/MachMsg/mig/MIGPayload.swift
@@ -1,6 +1,20 @@
 import Foundation
 import MachO
 
-public protocol MIGPayload: MachMessagePayload {
+/// A payload for a MIG message.
+public protocol MIGPayload: MachMessagePayload {}
+
+/// An empty payload.
+public struct EmptyMIGPayload: MIGPayload {
+    public func withUnsafeBytes<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
+        try body(UnsafeRawBufferPointer(start: nil, count: 0))  // pass an empty buffer
+    }
+    public init() {}
+}
+
+/// A payload for a MIG message containing an NDR record.
+public protocol MIGPayloadWithNDR: MIGPayload {
+    /// The NDR record for the payload.
+    /// - Important: The NDR record must be the first field in the payload.
     var NDR: NDR_record_t { get }
 }
