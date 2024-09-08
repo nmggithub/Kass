16d96a04d0a30f73cc7cec8c5a8a73392a204560
Noah Gregory <noahmgregory@gmail.com>
2024-09-08 17:06:33 -0400

    fix typo in comment

--- Sources/Mach/Base/errors/KernelError.swift
+++ Sources/Mach/Base/errors/KernelError.swift
@@ -5,7 +5,7 @@ extension Mach {
     public class KernelError: NSError {
         /// The kernel return code.
         public var kernelReturn: KernReturn { KernReturn(rawValue: Int32(self.code)) ?? .unknown }
-        /// Initializes a new kernel error from a kernel return code.
+        /// Initialize a new kernel error from a kernel return code.
         /// - Parameter kr: The kernel return code.
         public convenience init(_ kr: kern_return_t) {
             // Using the `NSMachErrorDomain` domain seems to cause NSError to automatically generate a proper error description.
