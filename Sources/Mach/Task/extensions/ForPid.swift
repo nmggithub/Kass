d4b8f1bf8fc1654b0bec2e250d4974585f7e858e
Noah Gregory <noahmgregory@gmail.com>
2024-09-08 16:11:15 -0400

    add task inspect for pid

--- Sources/Mach/Task/extensions/ForPid.swift
+++ Sources/Mach/Task/extensions/ForPid.swift
@@ -25,3 +25,15 @@ extension Mach.Task.NamePort {
         self.init(named: namePort)
     }
 }
+
+extension Mach.Task.InspectPort {
+    public convenience init(pid: pid_t) throws {
+        var namePort = task_name_t()
+        /// The first argument doesn't seem to be used anymore, but we pass in the current task name for historical reasons.
+        let ret = task_inspect_for_pid(Mach.Task.current.name, pid, &namePort)
+        guard ret == KERN_SUCCESS else {
+            throw NSError(domain: NSMachErrorDomain, code: Int(ret))
+        }
+        self.init(named: namePort)
+    }
+}
