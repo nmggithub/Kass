09b916b15ffd0d90829f84478fe853f045b4ba87
Noah Gregory <noahmgregory@gmail.com>
2024-09-15 16:01:27 -0400

    add Lifecycle2

---
+++ Sources/Mach/Task/Extensions/Lifecycle2.swift
@@ -0,0 +1,16 @@
+import Darwin.Mach
+
+extension Mach.Task {
+    /// A task suspension token.
+    public class SuspensionToken: Mach.Port {}
+    /// Suspend the task and receive a suspension token.
+    public func suspend2() throws -> SuspensionToken {
+        var token = task_suspension_token_t()
+        try Mach.Call(task_suspend2(self.name, &token))
+        return SuspensionToken(named: token)
+    }
+    /// Resume the task with a suspension token.
+    public func resume2(with token: SuspensionToken) throws {
+        try Mach.Call(task_resume2(token.name))
+    }
+}
