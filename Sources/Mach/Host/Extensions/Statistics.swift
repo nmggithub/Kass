8c4fb898b119ccc041e1025ce6209939b4ff449d
Noah Gregory <noahmgregory@gmail.com>
2024-09-18 01:18:10 -0400

    fix statistics, add info getter

--- Sources/Mach/Host/Extensions/Statistics.swift
+++ Sources/Mach/Host/Extensions/Statistics.swift
@@ -5,22 +5,17 @@ extension Mach.Host {
     public var statistics: Statistics { .init(for: self) }
     /// Statistics for a host.
     public class Statistics: Mach.FlavoredDataManagerNoAdditionalArgs<
-        Statistics.Flavor, host_info_t.Pointee
+        Statistics.Flavor, host_info_t.Pointee  // statistics use the same type as info
     >
     {
         /// A flavor of host statistics.
         public enum Flavor: host_flavor_t {
-            case basic = 1
-            case scheduling = 3
-            case resourceSizes = 4
-            case priority = 5
-            case semaphoreTraps = 7
-            case machMsgTraps = 8
-            case vmPurgeable = 9
-            case debugInfo = 10
-            /// - Note: Yes, this is what it's actually called.
-            case canHasDebugger = 11
-            case preferredUserspaceArchitecture = 12
+            case load = 1
+            case vm = 2
+            case cpuLoad = 3
+            case vm64 = 4
+            case extMod = 5
+            case expiredTasks = 6
         }
         /// Create a new statistics manager for the host.
         /// - Parameter host: The host to manage statistics for.
@@ -29,7 +24,12 @@ extension Mach.Host {
             self.init(
                 getter: {
                     flavor, array, count, _ in
-                    host_statistics(host.name, flavor.rawValue, array, &count)
+                    switch flavor {
+                    case .load, .vm, .cpuLoad, .expiredTasks:
+                        host_statistics(host.name, flavor.rawValue, array, &count)
+                    case .vm64, .extMod:
+                        host_statistics64(host.name, flavor.rawValue, array, &count)
+                    }
                 },
                 setter: {
                     _, _, _, _ in
