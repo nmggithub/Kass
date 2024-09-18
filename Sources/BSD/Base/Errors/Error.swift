fabaeb209010b3345cf7e1cd2e040ab5cc3063cf
Noah Gregory <noahmgregory@gmail.com>
2024-09-17 22:59:27 -0400

    explicitly pass errno instead of attempting to use default argument value

--- Sources/BSD/Base/Errors/Error.swift
+++ Sources/BSD/Base/Errors/Error.swift
@@ -4,9 +4,10 @@ import System
 extension BSD {
     /// An error that occurred in the BSD kernel.
     public class Error: NSError, @unchecked Sendable {
+        /// The error code as an `Errno`.
         public var typedErrno: Errno? { Errno(rawValue: Int32(self.code)) }
-        /// Initialize a new kernel error, defaulting to `errno`.
-        public convenience init(_ code: Int32 = errno) {
+        /// Initialize a new kernel error with a given code.
+        public convenience init(_ code: Int32) {
             self.init(domain: NSPOSIXErrorDomain, code: Int(code))
         }
     }
--- Sources/BSD/Base/Errors/Syscall.swift
+++ Sources/BSD/Base/Errors/Syscall.swift
@@ -1,3 +1,5 @@
+import Darwin.POSIX
+
 extension BSD {
     /// Execute a system call and throw an error if it fails.
     /// - Parameter syscall: A statement that executes a syscall and returns the result.
@@ -7,7 +9,7 @@ extension BSD {
         switch ret {
         case -1:
             // While it's not a standard, a return value of -1 usually indicates an error with `errno` set.
-            throw BSD.Error()  // passing no return code defaults to `errno`
+            throw BSD.Error(errno)
         case 0:
             return  // success
         default:
