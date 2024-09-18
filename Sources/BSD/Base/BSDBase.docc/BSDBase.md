1399efb351ac0a12dfca7f5c6c7167761dfab16c
Noah Gregory <noahmgregory@gmail.com>
2024-09-17 22:49:09 -0400

    add CSR support

---
+++ Sources/BSD/Base/BSDBase.docc/BSDBase.md
@@ -0,0 +1,26 @@
+# ``BSDBase``
+
+@Metadata {
+    @TitleHeading("Module")
+}
+
+## Licenses
+
+Portions of the code for this module are covered under license. Please view the source code for additional information.
+
+- <doc:APSL>
+
+## Topics
+
+
+### The BSD kernel
+
+- ``BSD``
+
+### Handling errors
+
+- ``BSD/Syscall(_:)``
+- ``BSD/Error``
+### Licenses
+
+- <doc:APSL>
---
+++ Sources/BSD/Base/CSR.swift
@@ -0,0 +1,74 @@
+/*
+ * Portions Copyright (c) 2014 Apple Inc. All rights reserved.
+ *
+ * The main difference between this file and the original file is that the original file is written in C, while this file is written in Swift.
+ *
+ * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
+ *
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. The rights granted to you under the License
+ * may not be used to create, or enable the creation or redistribution of,
+ * unlawful or unlicensed copies of an Apple operating system, or to
+ * circumvent, violate, or enable the circumvention or violation of, any
+ * terms of an Apple operating system software license agreement.
+ *
+ * Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this file.
+ *
+ * The Original Code and all software distributed under the License are
+ * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
+ */
+
+import Linking
+
+private let csr_check: @convention(c) (UInt32) -> Int32 = libSystem().get(symbol: "csr_check")!
+    .cast()
+private let csr_get_active_config: @convention(c) (UnsafeMutablePointer<UInt32>) -> Int32 =
+    libSystem().get(symbol: "csr_get_active_config")!.cast()
+
+extension BSD {
+    /// Configurable Security Restrictions.
+    /// - Important: This is more commonly known as System Integrity Protection (SIP).
+    /// - Warning: This work is covered under license. Please view the source code and <doc:BSDBase#Licenses> for more information.
+    public struct CSR: Namespace {
+        /// A configuration option for CSR.
+        public enum ConfigOption: UInt32, CaseIterable {
+            case allowUntrustedKexts = 1
+            case allowUnrestrictedFS = 2
+            case allowTaskForPid = 4
+            case allowKernelDebugger = 8
+            case allowAppleInternal = 16
+            case allowUnrestrictedDTrace = 32
+            case allowUnrestrictedNVRAM = 64
+            case allowDeviceConfiguration = 128
+            case allowAnyRecoveryOS = 256
+            case allowUnapprovedKexts = 512
+            case allowExecutionPolicyOverride = 1024
+            case allowUnauthenticatedRoot = 2048
+        }
+        /// Check a set of options.
+        /// - Parameter options: The options to check.
+        /// - Throws: An error if the options are not all in the active configuration.
+        public func Check(_ options: Set<ConfigOption>) throws {
+            let flags = options.reduce(0) { $0 | $1.rawValue }
+            try Syscall(csr_check(flags))
+        }
+        /// The active configuration.
+        public var activeConfig: Set<ConfigOption> {
+            get throws {
+                var flags: UInt32 = 0
+                try Syscall(csr_get_active_config(&flags))
+                return Set(ConfigOption.allCases.filter { flags & $0.rawValue != 0 })
+            }
+        }
+    }
+}
