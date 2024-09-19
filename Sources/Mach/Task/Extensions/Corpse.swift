efb15fdb7320efba5e8675c82afd1f1f86a1b4c5
Noah Gregory <noahmgregory@gmail.com>
2024-09-19 17:55:50 -0400

    don't worry about 32-bit systems

--- Sources/Mach/Task/Extensions/Corpse.swift
+++ Sources/Mach/Task/Extensions/Corpse.swift
@@ -7,35 +7,18 @@ extension Mach.Task {
         /// Information about the corpse.
         public var corpseInfo: Data {
             get throws {
-                switch Int.bitWidth {
-                case 32:
-                    var address = vm_address_t()
-                    var size = vm32_size_t()
-                    try Mach.call(
-                        task_map_corpse_info(self.owningTask.name, self.name, &address, &size)
-                    )
-                    guard let addressPointer = UnsafeRawPointer(bitPattern: Int(address)) else {
-                        fatalError("`task_map_corpse_info` returned a null pointer.")
-                    }
-                    return Data(
-                        bytes: addressPointer,
-                        count: Int(size)
-                    )
-                case 64:
-                    var address = mach_vm_address_t()
-                    var size = mach_vm_size_t()
-                    try Mach.call(
-                        task_map_corpse_info_64(self.owningTask.name, self.name, &address, &size)
-                    )
-                    guard let addressPointer = UnsafeRawPointer(bitPattern: Int(address)) else {
-                        fatalError("`task_map_corpse_info_64` returned a null pointer.")
-                    }
-                    return Data(
-                        bytes: addressPointer,
-                        count: Int(size)
-                    )
-                default: fatalError("Unsupported bit width.")
+                var address = mach_vm_address_t()
+                var size = mach_vm_size_t()
+                try Mach.call(
+                    task_map_corpse_info_64(self.owningTask.name, self.name, &address, &size)
+                )
+                guard let addressPointer = UnsafeRawPointer(bitPattern: Int(address)) else {
+                    fatalError("`task_map_corpse_info_64` returned a null pointer.")
                 }
+                return Data(
+                    bytes: addressPointer,
+                    count: Int(size)
+                )
             }
         }
     }
