9765e8e13b230a222ab71763fcea76744af59b15
Noah Gregory <noahmgregory@gmail.com>
2024-09-19 13:19:13 -0400

    improve task identity tokens

--- Sources/Mach/Task/Extensions/IdentityToken.swift
+++ Sources/Mach/Task/Extensions/IdentityToken.swift
@@ -1,6 +1,7 @@
 import Darwin.Mach
 
 extension Mach.Task {
+    public var identityToken: IdentityToken { get throws { try IdentityToken(for: self) } }
     /// A task's identity token.
     public class IdentityToken: Mach.Port {
         /// Get the identity token for a task.
@@ -13,7 +14,8 @@ extension Mach.Task {
         /// Use the identity token to get the task port for a given flavor.
         /// - Parameter flavor: The flavor of the task port to get.
         /// - Returns: The task port.
-        public func taskPort(flavor: Flavor) throws -> Mach.Task & Flavored {
+        /// - Warning: You must cast the returned task port to the correct subclass.
+        public func taskPort(_ flavor: Flavor) throws -> Mach.Task & Flavored {
             var taskPortName = mach_port_name_t()
             try Mach.call(
                 task_identity_token_get_task_port(self.name, flavor.rawValue, &taskPortName)
@@ -28,6 +30,23 @@ extension Mach.Task {
             case .name:
                 return Mach.Task.NamePort(named: taskPortName)
             }
+        }
+
+        /// The control port of the task the identity token is for.
+        public var taskControlPort: ControlPort {
+            get throws { try taskPort(.control) as! ControlPort }
+        }
+        /// The read port of the task the identity token is for.
+        public var taskReadPort: ReadPort {
+            get throws { try taskPort(.read) as! ReadPort }
+        }
+        /// The inspect port of the task the identity token is for.
+        public var taskInspectPort: InspectPort {
+            get throws { try taskPort(.inspect) as! InspectPort }
+        }
+        /// The name port of the task the identity token is for.
+        public var taskNamePort: NamePort {
+            get throws { try taskPort(.name) as! NamePort }
         }
     }
 }
