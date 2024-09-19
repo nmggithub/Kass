be1cc72ab643465bb1608eb4c78ad870ccaab30c
Noah Gregory <noahmgregory@gmail.com>
2024-09-19 14:01:15 -0400

    make ports getter throwable

--- Sources/Mach/Port/Task.swift
+++ Sources/Mach/Port/Task.swift
@@ -12,24 +12,24 @@ extension Mach {
         public var isSelf: Bool { mach_task_is_self(self.name) != 0 ? true : false }
         /// The ports in the task's namespace.
         public var ports: [Mach.Port] {
-            var namesCount = mach_msg_type_number_t.max
-            var names: mach_port_name_array_t? = mach_port_name_array_t.allocate(
-                capacity: Int(namesCount)
-            )
-            // the types array is not used, but it is required by `mach_port_names`
-            var typesCount = mach_msg_type_number_t.max
-            var types: mach_port_type_array_t? = mach_port_type_array_t.allocate(
-                capacity: Int(typesCount)
-            )
-            do {
+            get throws {
+                var namesCount = mach_msg_type_number_t.max
+                var names: mach_port_name_array_t? = mach_port_name_array_t.allocate(
+                    capacity: Int(namesCount)
+                )
+                // the types array is not used, but it is required by `mach_port_names`
+                var typesCount = mach_msg_type_number_t.max
+                var types: mach_port_type_array_t? = mach_port_type_array_t.allocate(
+                    capacity: Int(typesCount)
+                )
                 try Mach.call(
                     mach_port_names(self.name, &names, &namesCount, &types, &typesCount)
                 )
-            } catch { return [] }
-            return (0..<Int(namesCount)).map {
-                let port = Mach.Port(named: names![$0])
-                port.owningTask = self
-                return port
+                return (0..<Int(namesCount)).map {
+                    let port = Mach.Port(named: names![$0])
+                    port.owningTask = self
+                    return port
+                }
             }
         }
     }
