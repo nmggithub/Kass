57d9d2300ee1ef63a68e0d87a84a6b4774eb1dc3
Noah Gregory <noahmgregory@gmail.com>
2024-09-19 17:43:35 -0400

    add voucher support to tasks

---
+++ Sources/Mach/Task/Extensions/Voucher.swift
@@ -0,0 +1,28 @@
+import Darwin.Mach
+import MachVoucher
+
+extension Mach.Task {
+    /// Set the voucher for the task.
+    /// - Parameter voucher: The voucher to set.
+    /// - Throws: An error if the voucher could not be set.
+    public func setVoucher(_ voucher: Mach.Voucher) throws {
+        try Mach.call(task_set_mach_voucher(self.name, voucher.name))
+    }
+    /// Get the voucher for the task.
+    /// - Throws: An error if the voucher could not be retrieved.
+    /// - Returns: The voucher.
+    public func getVoucher() throws -> Mach.Voucher {
+        var voucherName: mach_voucher_name_t = MACH_VOUCHER_NAME_NULL
+        try Mach.call(task_get_mach_voucher(self.name, 0, &voucherName))  // the second parameter is no longer used
+        return Mach.Voucher(named: voucherName)
+    }
+    /// Swap the voucher for the task.
+    /// - Parameter voucher: The new voucher.
+    /// - Throws: An error if the voucher could not be swapped.
+    /// - Returns: The old voucher.
+    public func swapVoucher(_ voucher: Mach.Voucher) throws -> Mach.Voucher {
+        var voucherName: mach_voucher_name_t = MACH_VOUCHER_NAME_NULL
+        try Mach.call(task_swap_mach_voucher(self.name, voucher.name, &voucherName))
+        return Mach.Voucher(named: voucherName)
+    }
+}
