fc64e4516ca0512c9226b1e2b6f16b58974a072c
Noah Gregory <noahmgregory@gmail.com>
2024-09-20 21:28:16 -0400

    add finder info parsers

---
+++ Sources/BSD/FS/Attributes/Parsing/FinderInfo.swift
@@ -0,0 +1,49 @@
+import Carbon
+
+extension BSD.FS {
+    public struct FinderFileInfo {
+        public let info: FndrFileInfo
+        public let extendedInfo: FndrExtendedFileInfo
+    }
+    public struct FinderDirectoryInfo {
+        public let fileInfo: FndrDirInfo
+        public let folderInfo: FndrExtendedDirInfo
+    }
+}
+
+extension BSD.FS.Attribute.Reference.Parser {
+    /// A parser for Finder Info about a file.
+    public static var finderFileInfo: BSD.FS.Attribute.Reference.Parser<BSD.FS.FinderFileInfo> {
+        BSD.FS.Attribute.Reference.Parser<BSD.FS.FinderFileInfo> { data in
+            guard
+                data.count == MemoryLayout<FndrFileInfo>.size
+                    + MemoryLayout<FndrExtendedFileInfo>.size
+            else {
+                fatalError("Invalid Finder Info data size")
+            }
+            let info = data.withUnsafeBytes { $0.load(as: FndrFileInfo.self) }
+            let extendedInfo = data.advanced(by: MemoryLayout<FndrFileInfo>.size).withUnsafeBytes {
+                $0.load(as: FndrExtendedFileInfo.self)
+            }
+            return BSD.FS.FinderFileInfo(info: info, extendedInfo: extendedInfo)
+        }
+    }
+    /// A parser for Finder Info about a directory.
+    public static var finderDirectoryInfo:
+        BSD.FS.Attribute.Reference.Parser<BSD.FS.FinderDirectoryInfo>
+    {
+        BSD.FS.Attribute.Reference.Parser<BSD.FS.FinderDirectoryInfo> { data in
+            guard
+                data.count == MemoryLayout<FndrDirInfo>.size
+                    + MemoryLayout<FndrExtendedDirInfo>.size
+            else {
+                fatalError("Invalid Finder Info data size")
+            }
+            let fileInfo = data.withUnsafeBytes { $0.load(as: FndrDirInfo.self) }
+            let folderInfo = data.advanced(by: MemoryLayout<FndrDirInfo>.size).withUnsafeBytes {
+                $0.load(as: FndrExtendedDirInfo.self)
+            }
+            return BSD.FS.FinderDirectoryInfo(fileInfo: fileInfo, folderInfo: folderInfo)
+        }
+    }
+}
