983e71d7f5fea0647680247b00627cd6b5cdcdba
Noah Gregory <noahmgregory@gmail.com>
2024-09-25 18:50:42 -0400

    adjust ForPID API

--- Sources/Mach/Task/Extensions/ForPID.swift
+++ Sources/Mach/Task/Extensions/ForPID.swift
@@ -2,52 +2,33 @@ import BSDBase
 import Darwin.Mach
 import Linking
 
-extension Mach.TaskControl {
-    /// Gets the task control port for a process.
-    public convenience init(for pid: pid_t) throws {
-        var controlPort = task_t()
-        /// The first parameter doesn't seem to be used anymore, but we pass in the current task port name for historical reasons.
-        try Mach.call(task_for_pid(Mach.Task.current.name, pid, &controlPort))
-        self.init(named: controlPort)
-    }
-}
-
 private let task_read_for_pid:
     @convention(c) (task_t, pid_t, UnsafeMutablePointer<task_read_t>) -> kern_return_t =
         libSystem()
         .get(symbol: "task_read_for_pid")!.cast()
-
-extension Mach.TaskRead {
-    /// Gets the task read port for a process.
-    public convenience init(for pid: pid_t) throws {
-        var namePort = task_read_t()
-        /// The first parameter doesn't seem to be used anymore, but we pass in the current task name for historical reasons.
-        try BSD.syscall(task_read_for_pid(Mach.Task.current.name, pid, &namePort))  // This is weirdly a BSD syscall, not a Mach call.
-        self.init(named: namePort)
-    }
-}
 
 private let task_inspect_for_pid:
     @convention(c) (task_t, pid_t, UnsafeMutablePointer<task_inspect_t>) -> kern_return_t =
         libSystem()
         .get(symbol: "task_inspect_for_pid")!.cast()
 
-extension Mach.TaskInspect {
-    /// Gets the task inspect port for a process.
-    public convenience init(for pid: pid_t) throws {
-        var namePort = task_inspect_t()
-        /// The first parameter doesn't seem to be used anymore, but we pass in the current task port name for historical reasons.
-        try BSD.syscall(task_inspect_for_pid(Mach.Task.current.name, pid, &namePort))  // This is weirdly a BSD syscall, not a Mach call.
-        self.init(named: namePort)
-    }
-}
-
-extension Mach.TaskName {
-    /// Gets the task name port for a process.
-    public convenience init(for pid: pid_t) throws {
-        var namePort = task_name_t()
-        /// The first parameter doesn't seem to be used anymore, but we pass in the current task port name for historical reasons.
-        try Mach.call(task_name_for_pid(Mach.Task.current.name, pid, &namePort))
-        self.init(named: namePort)
+extension Mach.Task {
+    /// Gets the task port for a process.
+    public static func `for`(pid: pid_t) throws -> Self {
+        var portName = mach_port_name_t()
+        // The first parameter doesn't seem to be used anymore, but we pass in the current task port name for historical reasons.
+        switch Self.self {
+        case is Mach.TaskName.Type:
+            try BSD.syscall(task_read_for_pid(Mach.Task.current.name, pid, &portName))  // This is weirdly a BSD syscall, not a Mach call.
+        case is Mach.TaskInspect.Type:
+            try BSD.syscall(task_inspect_for_pid(Mach.Task.current.name, pid, &portName))  // This is weirdly a BSD syscall, not a Mach call.
+        case is Mach.TaskName.Type:
+            try Mach.call(task_name_for_pid(Mach.Task.current.name, pid, &portName))
+        case is Mach.TaskControl.Type:  // We default to the control port.
+            fallthrough
+        default:
+            try Mach.call(task_for_pid(Mach.Task.current.name, pid, &portName))
+        }
+        return Self.init(named: portName)
     }
 }
