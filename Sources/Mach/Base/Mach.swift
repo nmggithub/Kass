b88ce7dbea90f5a4812038cdd7cef3c6018a6512
Noah Gregory <noahmgregory@gmail.com>
2024-09-26 10:13:33 -0400

    moving some stuff around

--- Sources/Mach/Base/Mach.swift
+++ Sources/Mach/Base/Mach.swift
@@ -1,4 +1,7 @@
-@_documentation(visibility:private)
+import Darwin.Mach
+import Foundation
+
+@_documentation(visibility: private)
 /// A non-instantiable struct acting as a namespace.
 public protocol Namespace {}
 
@@ -8,4 +11,16 @@ extension Namespace {
 }
 
 /// The Mach kernel.
-public struct Mach: Namespace {}
+public struct Mach: Namespace {
+    /// Calls the kernel and throws an error if the call fails.
+    public static func call(_ call: @autoclosure () -> kern_return_t) throws {
+        let kr = call()
+        guard kr == KERN_SUCCESS else {
+            guard let typedCode = MachError.Code(rawValue: kr) else {
+                // Let's try again with an `NSError`. We use `NSMachErrorDomain` because this is still a Mach error (we hope).
+                throw NSError(domain: NSMachErrorDomain, code: Int(kr))
+            }
+            throw MachError(typedCode)
+        }
+    }
+}
