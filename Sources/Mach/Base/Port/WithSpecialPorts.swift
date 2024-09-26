5d9493da6e703c2fdd99354f6d3ef290bb0928d7
Noah Gregory <noahmgregory@gmail.com>
2024-09-25 20:46:38 -0400

    improve special ports manager protocol

--- Sources/Mach/Base/Port/WithSpecialPorts.swift
+++ Sources/Mach/Base/Port/WithSpecialPorts.swift
@@ -11,7 +11,8 @@ extension Mach.Port {
         func setSpecialPort(_ specialPort: SpecialPort, to port: Mach.Port) throws
     }
     /// A special port for a port.
-    public protocol SpecialPortType: RawRepresentable where RawValue == Int32 {
+    public protocol SpecialPortType: RawRepresentable
+    where RawValue == Int32, ParentPort.SpecialPort == Self {
         /// The parent port type.
         associatedtype ParentPort: Mach.Port.WithSpecialPorts
 
@@ -22,5 +23,19 @@ extension Mach.Port {
 
         /// Sets a special port.
         func set(for parentPort: ParentPort, to port: Mach.Port) throws
+    }
+}
+
+extension Mach.Port.SpecialPortType {
+    /// Gets a special port.
+    public func get<PortType: Mach.Port>(
+        for parentPort: ParentPort, as type: PortType.Type = PortType.self
+    ) throws -> PortType {
+        try parentPort.getSpecialPort(self, as: type)
+    }
+
+    /// Sets a special port.
+    public func set(for parentPort: ParentPort, to port: Mach.Port) throws {
+        try parentPort.setSpecialPort(self, to: port)
     }
 }
