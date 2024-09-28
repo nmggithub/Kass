ec81cd91ad511325cf5d761e77213900f72883e5
Noah Gregory <noahmgregory@gmail.com>
2024-09-28 17:18:37 -0400

    remove redundant word

--- Sources/MachC/include/dyld_extra.h
+++ Sources/MachC/include/dyld_extra.h
@@ -1,2 +1,2 @@
-// The official macOS SDK's don't expose this to Swift, so we need to do it manually.
+// The macOS SDK's don't expose this to Swift, so we need to do it manually.
 #include <mach-o/dyld_images.h>
