f01703931bcfc1704ed53d8c5aadb316f180036a
Noah Gregory <noahmgregory@gmail.com>
2024-09-28 17:14:30 -0400

    clarify comment

--- Sources/MachC/include/dyld_extra.h
+++ Sources/MachC/include/dyld_extra.h
@@ -1,2 +1,2 @@
-// The official macOS SDK's don't include this, so we include it here.
+// The official macOS SDK's don't expose this to Swift, so we need to do it manually.
 #include <mach-o/dyld_images.h>
