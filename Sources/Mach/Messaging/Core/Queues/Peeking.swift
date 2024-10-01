aa2f050f3ab4da2ecbf62a9098613fcacd48b0f1
Noah Gregory <noahmgregory@gmail.com>
2024-10-01 15:26:33 -0400

    fix message peeking

--- Sources/Mach/Messaging/Core/Queues/Peeking.swift
+++ Sources/Mach/Messaging/Core/Queues/Peeking.swift
@@ -1,6 +1,10 @@
 import Darwin.Mach
 
-extension Mach.MessageServer {
+// These were "functional" C macros in the original code, but Swift doesn't support those.
+private func MACH_RCV_TRAILER_ELEMENTS(_ x: Int32) -> Int32 { ((x) & 0xf) << 24 }
+private func MACH_RCV_TRAILER_TYPE(_ x: Int32) -> Int32 { ((x) & 0xf) << 28 }
+
+extension Mach {
     /// A peeked preview of a message.
     public struct PeekedMessage {
         /// The advertised size of the message.
@@ -10,22 +14,25 @@ extension Mach.MessageServer {
         public var id: mach_msg_id_t
 
         /// The trailer.
-        public var trailer: mach_msg_max_trailer_t
+        public var trailer: mach_msg_audit_trailer_t
     }
+}
 
-    /// Peeks at the message in the queue with the given sequence number, receiving a trailer of the given type.
-    public func peek(
-        trailerType: Mach.Message.Trailer.TrailerType,
-        sequenceNumber: inout mach_port_seqno_t
-    ) throws -> PeekedMessage {
+extension Mach.MessageServer {
+    /// Peeks at the message in the queue with the given sequence number.
+    public func peek(sequenceNumber: inout mach_port_seqno_t) throws -> Mach.PeekedMessage {
         var messageSize = mach_msg_size_t(0)
         var messageID = mach_msg_id_t(0)
-        let trailer = try Mach.callWithCountOut(type: mach_msg_max_trailer_t.self) {
+        let trailer = try Mach.callWithCountInOut(type: mach_msg_audit_trailer_t.self) {
             trailerInfo, count in
             mach_port_peek(
                 self.owningTask.name,
                 self.name,
-                trailerType.rawValue,
+                // We peek as much of the trailer as the kernel will let us (which is everything up to, and including, to the audit token).
+                mach_msg_trailer_type_t(
+                    MACH_RCV_TRAILER_TYPE(MACH_MSG_TRAILER_FORMAT_0)  // This results in 0, so it's not strictly necessary, but this is the documented way to do it.
+                        | MACH_RCV_TRAILER_ELEMENTS(MACH_RCV_TRAILER_AUDIT)
+                ),
                 &sequenceNumber,
                 &messageSize,
                 &messageID,
@@ -33,7 +40,7 @@ extension Mach.MessageServer {
                 &count
             )
         }
-        return PeekedMessage(
+        return Mach.PeekedMessage(
             size: messageSize,
             id: messageID,
             trailer: trailer
