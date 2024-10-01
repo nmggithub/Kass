da1b2a8d2cedc6ce25d63af7186a2cb419835b03
Noah Gregory <noahmgregory@gmail.com>
2024-10-01 08:33:19 -0400

    use private header instead for TaskPolicyPrivate

--- Sources/Mach/Core/Task/TaskPolicyPrivate.swift
+++ Sources/Mach/Core/Task/TaskPolicyPrivate.swift
@@ -1,85 +1,19 @@
-/*
- * Portions Copyright (c) 2000-2005 Apple Computer, Inc. All rights reserved.
- *
- * The structures for these policies are not included in the public
- * header files. They are taken from the XNU source code.
- *
- * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
- *
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. The rights granted to you under the License
- * may not be used to create, or enable the creation or redistribution of,
- * unlawful or unlicensed copies of an Apple operating system, or to
- * circumvent, violate, or enable the circumvention or violation of, any
- * terms of an Apple operating system software license agreement.
- *
- * Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this file.
- *
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- *
- * @APPLE_OSREFERENCE_LICENSE_HEADER_END@
- */
-
-extension Mach {
-    /// A task's suppression policy.
-    /// - Warning: This work is covered under license. Please view the source code and <doc:MachCore#Licenses> for more information.
-    public struct TaskSuppressionPolicy: BitwiseCopyable {
-        let active: Int32
-        let lowpri_cpu: Int32
-        let timer_throttle: Int32
-        let disk_throttle: Int32
-        let cpu_limit: Int32
-        let suspend: Int32
-        let throughput_qos: Int32
-        let suppressed_cpu: Int32
-        let background_sockets: Int32
-        let reserved1: Int32
-        let reserved2: Int32
-        let reserved3: Int32
-        let reserved4: Int32
-        let reserved5: Int32
-        let reserved6: Int32
-        let reserved7: Int32
-    }
-
-    /// A task's policy state.
-    /// - Warning: This work is covered under license. Please view the source code and <doc:MachCore#Licenses> for more information.
-    public struct TaskPolicyState: BitwiseCopyable {
-        let requested: UInt64
-        let effective: UInt64
-        let pending: UInt64
-        let imp_assertcnt: UInt32
-        let imp_externcnt: UInt32
-        let flags: UInt64
-        let imp_transitions: UInt64
-        let tps_requested_policy: UInt64
-        let tps_effective_policy: UInt64
-    }
-}
+import MachC.TaskPolicyPrivate
 
 extension Mach.TaskPolicyManager {
     /// The task's suppression policy.
-    public var suppressionPolicy: Mach.TaskSuppressionPolicy {
+    public var suppressionPolicy: task_suppression_policy {
         get throws { try self.get(.suppression) }
     }
 
     /// Set's the task's suppression policy.
-    public func setSuppressionPolicy(to suppressionPolicy: Mach.TaskSuppressionPolicy) throws {
+    public func setSuppressionPolicy(to suppressionPolicy: task_suppression_policy) throws {
         try self.set(.suppression, to: suppressionPolicy)
     }
 
     /// The task's policy state.
     /// - Important: Only privileged tasks can get this.
-    public var policyState: Mach.TaskPolicyState {
+    public var policyState: task_policy_state {
         get throws { try self.get(.state) }
     }
 }
--- Sources/MachC/include/mk_timer.h
+++ Sources/MachC/include/mk_timer.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2000-2005 Apple Computer, Inc. All rights reserved.
  *
- * Original: https://github.com/apple-oss-distributions/xnu/blob/xnu-10063.101.15/osfmk/mach/mk_timer.h
+ * Original: https://github.com/apple-oss-distributions/xnu/blob/xnu-11215.1.10/osfmk/mach/mk_timer.h
  * No modifications have made from the original source code except for this line and the preceding one.
  * 
  * @APPLE_OSREFERENCE_LICENSE_HEADER_START@
@@ -45,35 +45,36 @@
 
 __BEGIN_DECLS
 
-mach_port_name_t        mk_timer_create(void);
+extern mach_port_name_t mk_timer_create(void);
 
-kern_return_t   mk_timer_destroy(
+extern kern_return_t    mk_timer_destroy(
 	mach_port_name_t        name);
 
-kern_return_t   mk_timer_arm(
+extern kern_return_t    mk_timer_arm(
 	mach_port_name_t        name,
-	uint64_t                        expire_time);
+	uint64_t                expire_time);
 
-kern_return_t   mk_timer_cancel(
+extern kern_return_t    mk_timer_cancel(
 	mach_port_name_t        name,
-	uint64_t                        *result_time);
-#define MK_TIMER_NORMAL (0)
-#define MK_TIMER_CRITICAL (1)
-kern_return_t   mk_timer_arm_leeway(
+	uint64_t               *result_time);
+
+/* mk_timer_flags */
+#define MK_TIMER_NORMAL         (0)
+#define MK_TIMER_CRITICAL       (1)
+
+extern kern_return_t    mk_timer_arm_leeway(
 	mach_port_name_t        name,
 	uint64_t                mk_timer_flags,
 	uint64_t                mk_timer_expire_time,
 	uint64_t                mk_timer_leeway);
 
-__END_DECLS
-
 #pragma pack(4)
-struct mk_timer_expire_msg {
+typedef struct mk_timer_expire_msg {
 	mach_msg_header_t       header;
-	uint64_t                        unused[3];
-};
+	uint64_t                unused[3];
+} mk_timer_expire_msg_t;
 #pragma pack()
 
-typedef struct mk_timer_expire_msg              mk_timer_expire_msg_t;
+__END_DECLS
 
 #endif /* _MACH_MK_TIMER_H_ */
