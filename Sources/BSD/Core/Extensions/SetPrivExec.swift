7b3837b8175429324efea8ee676e7f58ade922c9
Noah Gregory <noahmgregory@gmail.com>
2024-12-12 12:30:14 -0500

    add setprivexec syscall

---
+++ Sources/BSD/Core/Extensions/SetPrivExec.swift
@@ -0,0 +1,14 @@
+import Linking
+
+typealias setprivexec_f = @convention(c) (Int32) -> Int32
+let setprivexec: setprivexec_f = libSystem().get(symbol: "setprivexec")!.cast()
+
+extension BSD {
+    /// Allows or disallows the process the be a debugger.
+    /// - Note: The flag that this sets appears to be mostly unused in the kernel proper.
+    public func setPrivExec(_ state: Bool) throws {
+        try BSD.syscall(
+            setprivexec(state ? 1 : 0)
+        )
+    }
+}
