e6c2de5ecfe764b093692e7885e6b1ddcd025222
Noah Gregory <noahmgregory@gmail.com>
2025-03-28 21:41:07 -0400

    add MIGPayloadPrefixedWithNDR

--- Sources/Mach/Core/Messaging/MIG/MIGPayload.swift
+++ Sources/Mach/Core/Messaging/MIG/MIGPayload.swift
@@ -12,6 +12,49 @@ extension Mach {
         var NDR: NDR_record_t { get }
     }
 
+    /// A payload for a MIG message containing an NDR record and an inner payload.
+    public struct MIGPayloadPrefixedWithNDR<InnerPayload: BitwiseCopyable>: Mach.MIGPayload {
+
+        /// The NDR record for the payload.
+        public let NDR: NDR_record_t
+
+        /// The inner payload.
+        public let innerPayload: InnerPayload
+
+        /// Creates a new payload with an NDR record and an inner payload.
+        public init(
+            NDR: NDR_record_t = NDR_record_t(),
+            innerPayload: InnerPayload
+        ) {
+            self.NDR = NDR
+            self.innerPayload = innerPayload
+        }
+
+        public static func fromRawPayloadBuffer(
+            _ buffer: UnsafeRawBufferPointer
+        ) -> Mach.MIGPayloadPrefixedWithNDR<InnerPayload>? {
+            let ndr = buffer.load(fromByteOffset: 0, as: NDR_record_t.self)
+            let innerPayload = buffer.load(
+                fromByteOffset: MemoryLayout<NDR_record_t>.size, as: InnerPayload.self)
+            return Self(NDR: ndr, innerPayload: innerPayload)
+        }
+
+        public func toRawPayloadBuffer() -> UnsafeRawBufferPointer {
+            let size = MemoryLayout<NDR_record_t>.size + MemoryLayout<InnerPayload>.size
+            let buffer = UnsafeMutableRawBufferPointer.allocate(
+                byteCount: size,
+                alignment: MemoryLayout<InnerPayload>.alignment
+            )
+            buffer.storeBytes(of: NDR, toByteOffset: 0, as: NDR_record_t.self)
+            buffer.storeBytes(
+                of: innerPayload,
+                toByteOffset: MemoryLayout<NDR_record_t>.size,
+                as: InnerPayload.self
+            )
+            return UnsafeRawBufferPointer(buffer)
+        }
+    }
+
     /// A payload for a MIG message containing only an NDR record.
     public struct MIGPayloadWithOnlyNDR: Mach.MIGPayloadWithNDR {
         public var NDR: NDR_record_t
