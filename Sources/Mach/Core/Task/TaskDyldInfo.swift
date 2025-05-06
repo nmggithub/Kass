ba424ee50a6df3792e5e1a000241b49c72a0cbca
Noah Gregory <noahmgregory@gmail.com>
2025-05-06 00:23:17 -0400

    fix bug for getting the dyld info of other tasks

--- Sources/Mach/Core/Task/TaskDyldInfo.swift
+++ Sources/Mach/Core/Task/TaskDyldInfo.swift
@@ -15,53 +15,10 @@ extension dyld_image_mode {
     public static let dyldMoved = dyld_image_dyld_moved
 }
 
-/// Adds properties to make the `dyld_image_info` struct more Swift-friendly.
-extension dyld_image_info {
-    public var imageFilePathString: String { String(cString: self.imageFilePath) }
-}
-
-/// Adds properties to make the `dyld_aot_image_info` struct more Swift-friendly.
-extension dyld_aot_image_info {
-    public var aotImageKeyData: Data {
-        withUnsafeBytes(of: self.aotImageKey) { bytes in Data(bytes) }
-    }
-}
-
-/// Adds properties to make the `dyld_all_image_infos` struct more Swift-friendly.
-extension dyld_all_image_infos {  // Note: The availability constraints are based on comments in the original header file.
-    public var infos: [dyld_image_info] {
-        guard let infoArray = self.infoArray else { return [] }
-        return (0..<Int(self.infoArrayCount)).map { index in infoArray[index] }
-    }
-    public func notify(_ mode: dyld_image_mode, _ infos: consuming [dyld_image_info]) {
-        self.notification(mode, UInt32(infos.count), infos)
-    }
-    @available(macOS, introduced: 10.6)
-    public var dyldVersionString: String? {
-        guard let actualDyldVersion = self.dyldVersion else { return nil }
-        return String(cString: actualDyldVersion)
-    }
-    @available(macOS, introduced: 10.6)
-    public var errorMessageString: String? {
-        guard let actualErrorMessage = self.errorMessage else { return nil }
-        return String(cString: actualErrorMessage)
-    }
-    @available(macOS, introduced: 10.6)
-    public var uuids: [dyld_uuid_info] {
-        guard let uuidArray = self.uuidArray else { return [] }
-        return (0..<Int(self.uuidArrayCount)).map { index in uuidArray[index] }
-    }
-    @available(macOS, introduced: 10.13)
-    public var compactDyldImageInfo: UnsafeRawBufferPointer {
-        UnsafeRawBufferPointer(
-            start: UnsafeRawPointer(bitPattern: self.compact_dyld_image_info_addr),
-            count: Int(self.compact_dyld_image_info_size)
-        )
-    }
-}
-
 extension Mach.TaskInfoManager {
     /// Information about `dyld` images in the task.
+    /// - Warning: All pointers in this structure are valid only in the target task's address space. If the target
+    ///     task is not the current task, you should use the virtual memory API to read the data at such pointers.
     public var dyldInfo: dyld_all_image_infos {
         get throws {
             let dyldInfo: task_dyld_info = try self.get(.dyld)
@@ -71,7 +28,16 @@ extension Mach.TaskInfoManager {
             else {
                 fatalError("The size of the `dyld` info struct doesn't match the expected size.")
             }
-            return infoPointer.load(as: dyld_all_image_infos.self)
+
+            // The pointer is to memory inside the target task's address space, so we need to
+            //  use the virtual memory API to ensure that we read the proper data. This might
+            //  be overkill if the target task is the current task, but it doesn't hurt to be
+            //  safe. The below code will work regardless of the target task, though it won't
+            //  change the values of any pointers in the struct itself. The user will have to
+            //  use the virtual memory API to read those pointers if they want to use them.
+            return try self.task.vm.read(
+                from: infoPointer.assumingMemoryBound(to: dyld_all_image_infos.self)
+            )
         }
     }
 }
