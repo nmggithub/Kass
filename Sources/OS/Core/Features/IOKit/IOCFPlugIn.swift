2ee247aeb51013c3f1864ec8bdf0d5e9c0c64326
Noah Gregory <noahmgregory@gmail.com>
2025-08-28 14:57:51 -0400

    move IOKit API and add support for IOCFPlugIn's

---
+++ Sources/OS/Core/Features/IOKit/IOCFPlugIn.swift
@@ -0,0 +1,47 @@
+#if canImport(IOKit)
+
+    import IOKit
+    import MachCore
+
+    extension OS {
+        /// A plug-in interface (really a pointer to a pointer to an interface structure).
+        typealias IOCFPlugInInterface =
+            UnsafeMutablePointer<UnsafeMutablePointer<IOCFPlugInInterfaceStruct>?>
+    }
+
+    extension OS.IOCFPlugInInterface {
+        /// Returns the specified method in the plug-in interface so it can be called.
+        /// - Note: This is mainly to avoid having to use `pointee.pointee` everywhere.
+        /// - Warning: Only use this function to get methods, not properties.
+        /// - Warning: Immediately call the returned function. Do not store it for later.
+        func call<Path>(_ keyPath: KeyPath<IOCFPlugInInterfaceStruct, Path>) -> Path {
+            self.pointee!.pointee[keyPath: keyPath]
+        }
+
+        /// Destroys the plug-in interface.
+        func destroy() throws {
+            try Mach.call(IODestroyPlugInInterface(self))
+        }
+    }
+
+    extension OS.IOService {
+        /// Creates a plug-in interface for the specified service, returning the interface and a score.
+        func createPlugInInterface(
+            pluginType: CFUUID,
+            interfaceType: CFUUID
+        ) throws -> (interface: OS.IOCFPlugInInterface, score: Int32) {
+            var pluginInterface: OS.IOCFPlugInInterface?
+            var score: Int32 = 0
+            try Mach.call(
+                IOCreatePlugInInterfaceForService(
+                    self.name,
+                    pluginType,
+                    interfaceType,
+                    &pluginInterface,
+                    &score
+                ))
+            return (interface: pluginInterface!, score: score)
+        }
+    }
+
+#endif  // canImport(IOKit)
