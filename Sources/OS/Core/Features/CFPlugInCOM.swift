2b8c3a5efe5fa3ce4508d2f28dae84c86f8e5b01
Noah Gregory <noahmgregory@gmail.com>
2025-08-29 03:37:41 -0400

    refactor COM API wrappers

---
+++ Sources/OS/Core/Features/CFPlugInCOM.swift
@@ -0,0 +1,172 @@
+import CoreFoundation.CFPlugInCOM
+import KassHelpers
+
+extension OS {
+    /// COM-related functionality.
+    public struct COM: KassHelpers.Namespace {
+        /// A return value for a COM operation.
+        public struct HResult: RawRepresentable, Sendable {
+            /// The raw HRESULT value.
+            public var rawValue: HRESULT
+
+            /// Represents a raw HRESULT value.
+            public init(rawValue: HRESULT) {
+                self.rawValue = rawValue
+            }
+
+            /// If the operation succeeded.
+            // Based on the SUCCEEDED macro.
+            public var succeeded: Bool { return rawValue >= 0 }
+
+            /// If the operation failed.
+            // Based on the FAILED macro.
+            public var failed: Bool { return rawValue < 0 }
+
+            /// If the operation resulted in an error.
+            /// - Warning: While this is basically equivalent to `failed`, it is
+            ///     advised to use `failed` over this for canonical purposes.
+            // Based on the IS_ERROR macro.
+            public var isError: Bool { return severity == SEVERITY_ERROR }
+
+            /// The code.
+            // Based on the HRESULT_CODE macro.
+            public var code: Int32 { rawValue & 0xFFFF }
+
+            /// The facility.
+            // Based on the HRESULT_FACILITY macro.
+            public var facility: Int32 { (rawValue >> 16) & 0x1FFF }
+
+            /// The severity.
+            // Based on the HRESULT_SEVERITY macro.
+            public var severity: Int32 { (rawValue >> 31) & 0x1 }
+
+            /// Represents an HRESULT by its component parts.
+            // Based on the MAKE_HRESULT macro.
+            public init(severity: Int32, facility: Int32, code: Int32) {
+                self.rawValue = (severity << 31) | (facility << 16) | (code & 0xFFFF)
+            }
+        }
+
+        /// An error result from a COM operation.
+        public struct COMError: Error {
+            /// The underlying HRESULT value.
+            public var hresult: HResult
+
+            /// Represents the error associated with the HRESULT, if one exists.
+            public init?(hresult: HResult) {
+                guard hresult.failed else { return nil }
+                self.hresult = hresult
+            }
+        }
+
+        /// A protocol for a IUnknown vtable.
+        // This is just the IUnknownVtbl struct as a Swift protocol. We redefine
+        //  it here to make working with IUnknown vtables easier.
+        public protocol IUnknownVTblProtocol {
+            var _reserved: UnsafeMutableRawPointer! { get }
+            var QueryInterface:
+                (
+                    @convention(c) (
+                        UnsafeMutableRawPointer?, REFIID, UnsafeMutablePointer<LPVOID?>?
+                    ) ->
+                        HRESULT
+                )!
+            { get }
+            var AddRef: (@convention(c) (UnsafeMutableRawPointer?) -> ULONG)! { get }
+            var Release: (@convention(c) (UnsafeMutableRawPointer?) -> ULONG)! { get }
+        }
+
+        /// A pointer for a COM interface (really a pointer to a pointer to an interface structure).
+        public typealias COMInterfacePointer<ActualInterface: IUnknownVTblProtocol> =
+            UnsafeMutablePointer<UnsafeMutablePointer<ActualInterface>?>
+
+        /// A COM interface.
+        public protocol COMInterface {
+            /// The type of the interface's vtable,
+            associatedtype VTable: IUnknownVTblProtocol
+
+            /// The pointer to the interface.
+            var pointer: COMInterfacePointer<VTable> { get }
+
+            /// The UUID for the interface.
+            static var interfaceID: CFUUID { get }
+
+            /// The vtable for the interface.
+            var vtable: VTable { get }
+
+            /// Represents the interface pointed to by the pointer.
+            init(pointer: LPVOID)
+        }
+    }
+}
+
+extension OS.COM.COMInterface {
+    public var vtable: VTable {
+        self.pointer.pointee!.pointee
+    }
+
+    public init(pointer: LPVOID) {
+        self.init(
+            pointer: pointer.assumingMemoryBound(
+                to: OS.COM.COMInterfacePointer<VTable>.Pointee.self
+            )
+        )
+    }
+}
+
+/// Makes the IUnknownVtbl struct conform to IUnknownVTblProtocol.
+extension IUnknownVTbl: OS.COM.IUnknownVTblProtocol {}
+
+extension OS.COM {
+    /// An unknown COM interface.
+    public struct IUnknownCOMInterface: OS.COM.COMInterface {
+        public static var interfaceID: CFUUID {
+            // This is defined as macro in the original SDK. That macro
+            //  can't be used in Swift, so we redefine the value here.
+            CFUUIDGetConstantUUIDWithBytes(
+                kCFAllocatorSystemDefault,
+                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0xC0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46
+            )!
+        }
+        public var pointer: OS.COM.COMInterfacePointer<IUnknownVTbl>
+    }
+}
+
+/// Wraps the members of the COM interface's vtable.
+extension OS.COM.COMInterface {
+    /// Executes a function that returns a HRESULT and throws an error if it fails.
+    public func call(
+        _ call: @autoclosure () throws -> HRESULT
+    ) throws {
+        let hr = try call()
+        if let error = OS.COM.COMError(hresult: OS.COM.HResult(rawValue: hr)) {
+            throw error
+        }
+    }
+    /// Queries for an interface with the given UUID and returns a pointer to it.
+    public func QueryInterface<QueriedInterface: OS.COM.COMInterface>(
+        _ uuid: CFUUID,
+        as: QueriedInterface.Type = QueriedInterface.self
+    ) throws -> QueriedInterface {
+        var result: LPVOID?
+        try self.call(
+            self.vtable.QueryInterface(
+                self.pointer,
+                CFUUIDGetUUIDBytes(uuid),
+                &result
+            )
+        )
+        return QueriedInterface(pointer: result!)
+    }
+
+    /// Increments the reference count for the object and returns the new count.
+    public func AddRef() -> ULONG {
+        self.vtable.AddRef(self.pointer)
+    }
+
+    /// Decrements the reference count for the object and returns the new count.
+    public func Release() -> ULONG {
+        self.vtable.Release(self.pointer)
+    }
+}
--- Sources/OS/Core/Features/IOKit/IOCFPlugIn.swift
+++ Sources/OS/Core/Features/IOKit/IOCFPlugIn.swift
@@ -3,24 +3,29 @@
     import IOKit
     import MachCore
 
+    /// Makes IOCFPlugInInterfaceStruct conform to IUnknownVTblProtocol.
+    extension IOCFPlugInInterfaceStruct: OS.COM.IUnknownVTblProtocol {}
+
     extension OS {
         /// A plug-in interface (really a pointer to a pointer to an interface structure).
-        public typealias IOCFPlugInInterface =
-            UnsafeMutablePointer<UnsafeMutablePointer<IOCFPlugInInterfaceStruct>?>
+        public struct IOCFPlugInCOMInterface: OS.COM.COMInterface {
+            public var pointer: OS.COM.COMInterfacePointer<IOCFPlugInInterfaceStruct>
+            public static var interfaceID: CFUUID {
+                // This is defined as macro in the original SDK. That macro
+                //  can't be used in Swift, so we redefine the value here.
+                CFUUIDGetConstantUUIDWithBytes(
+                    nil,
+                    0xC2, 0x44, 0xE8, 0x58, 0x10, 0x9C, 0x11, 0xD4,
+                    0x91, 0xD4, 0x00, 0x50, 0xE4, 0xC6, 0x42, 0x6F
+                )
+            }
+        }
     }
 
-    extension OS.IOCFPlugInInterface {
-        /// Returns the specified method in the plug-in interface so it can be called.
-        /// - Note: This is mainly to avoid having to use `pointee.pointee` everywhere.
-        /// - Warning: Only use this function to get methods, not properties.
-        /// - Warning: Immediately call the returned function. Do not store it for later.
-        public func call<Path>(_ keyPath: KeyPath<IOCFPlugInInterfaceStruct, Path>) -> Path {
-            self.pointee!.pointee[keyPath: keyPath]
-        }
-
+    extension OS.IOCFPlugInCOMInterface {
         /// Destroys the plug-in interface.
         public func destroy() throws {
-            try Mach.call(IODestroyPlugInInterface(self))
+            try Mach.call(IODestroyPlugInInterface(self.pointer))
         }
     }
 
@@ -29,19 +34,46 @@
         public func createPlugInInterface(
             pluginType: CFUUID,
             interfaceType: CFUUID
-        ) throws -> (interface: OS.IOCFPlugInInterface, score: Int32) {
-            var pluginInterface: OS.IOCFPlugInInterface?
+        ) throws -> (interface: OS.IOCFPlugInCOMInterface, score: Int32) {
+            var pluginInterfacePointer: OS.COM.COMInterfacePointer<IOCFPlugInInterfaceStruct>?
             var score: Int32 = 0
             try Mach.call(
                 IOCreatePlugInInterfaceForService(
                     self.name,
                     pluginType,
                     interfaceType,
-                    &pluginInterface,
+                    &pluginInterfacePointer,
                     &score
                 ))
-            return (interface: pluginInterface!, score: score)
+            let pluginInterface = OS.IOCFPlugInCOMInterface(pointer: pluginInterfacePointer!)
+            return (interface: pluginInterface, score: score)
         }
     }
 
+    /// Wraps the members of the plug-in interface's vtable.
+    // First-party documentation on these members is barren, so the comments here are left intentionally vague.
+    extension OS.IOCFPlugInCOMInterface {
+        /// The version of the plug-in interface.
+        public var version: UInt16 { self.vtable.version }
+
+        /// The revision of the plug-in interface.
+        public var revision: UInt16 { self.vtable.revision }
+
+        /// Probes the plug-in interface.
+        public func Probe(propertyTable: CFDictionary, service: OS.IOService) throws -> Int32 {
+            var order: Int32 = 0
+            try Mach.call(self.vtable.Probe(self.pointer, propertyTable, service.name, &order))
+            return order
+        }
+
+        /// Starts the plug-in interface.
+        public func Start(propertyTable: CFDictionary, service: OS.IOService) throws {
+            try Mach.call(self.vtable.Start(self.pointer, propertyTable, service.name))
+        }
+
+        /// Stops the plug-in interface.
+        public func Stop() throws {
+            try Mach.call(self.vtable.Stop(self.pointer))
+        }
+    }
 #endif  // canImport(IOKit)
