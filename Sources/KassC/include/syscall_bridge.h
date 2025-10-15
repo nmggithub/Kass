5a91827626f69de6cfdfe2b6d637edeff6f7134f
Noah Gregory <noahmgregory@gmail.com>
2025-10-15 01:34:31 -0400

    remove inlining of syscall bridge functions

--- Sources/KassC/include/syscall_bridge.h
+++ Sources/KassC/include/syscall_bridge.h
@@ -8,47 +8,47 @@ int syscall(int number, ...);
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-inline int syscall0(int number)
+int syscall0(int number)
 {
     return syscall(number);
 }
 
-inline int syscall1(int number, syscall_arg_t arg1)
+int syscall1(int number, syscall_arg_t arg1)
 {
     return syscall(number, arg1);
 }
 
-inline int syscall2(int number, syscall_arg_t arg1, syscall_arg_t arg2)
+int syscall2(int number, syscall_arg_t arg1, syscall_arg_t arg2)
 {
     return syscall(number, arg1, arg2);
 }
 
-inline int syscall3(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3)
+int syscall3(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3)
 {
     return syscall(number, arg1, arg2, arg3);
 }
 
-inline int syscall4(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4)
+int syscall4(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4)
 {
     return syscall(number, arg1, arg2, arg3, arg4);
 }
 
-inline int syscall5(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5)
+int syscall5(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5)
 {
     return syscall(number, arg1, arg2, arg3, arg4, arg5);
 }
 
-inline int syscall6(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6)
+int syscall6(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6)
 {
     return syscall(number, arg1, arg2, arg3, arg4, arg5, arg6);
 }
 
-inline int syscall7(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6, syscall_arg_t arg7)
+int syscall7(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6, syscall_arg_t arg7)
 {
     return syscall(number, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
 }
 
-inline int syscall8(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6, syscall_arg_t arg7, syscall_arg_t arg8)
+int syscall8(int number, syscall_arg_t arg1, syscall_arg_t arg2, syscall_arg_t arg3, syscall_arg_t arg4, syscall_arg_t arg5, syscall_arg_t arg6, syscall_arg_t arg7, syscall_arg_t arg8)
 {
     return syscall(number, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
 }
